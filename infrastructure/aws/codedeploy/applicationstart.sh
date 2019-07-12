fac32d3144561e7313656d607eb698fcc549e259
Kiranzigma <kathiresan.k@husky.neu.edu>
2019-07-11 21:46:55 -0400

    App_Start_Updated

--- infrastructure/aws/codedeploy/applicationstart.sh
+++ infrastructure/aws/codedeploy/applicationstart.sh
@@ -5,7 +5,7 @@ echo "sudo yum install lsof -y" >> /tmp/tempFile.sh
 echo "APP_PORT=sudo lsof -t -i:8080" >> /tmp/tempFile.sh
 echo "sudo kill -9 \$APP_PORT" >> /tmp/tempFile.sh
 echo "cd /var" >> /tmp/tempFile.sh
-echo "sudo java -jar -Dspring.profiles.active=cloud -Drds.database=\$DB_HOST -Dbucketname=\$S3_BUCKET LMSApp-0.0.1-SNAPSHOT.war" >> /tmp/tempFile.sh
+echo "sudo java -jar -Dspring.profiles.active=cloud -Ddatabase=\$DB_HOST -Dbucketname=\$S3_BUCKET LMSApp-0.0.1-SNAPSHOT.war" >> /tmp/tempFile.sh
 cd /tmp/
 ./tempFile.sh > run.out 2> run.err &
 
