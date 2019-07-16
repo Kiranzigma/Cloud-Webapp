ac455f5e96234b596230b3c5009cf03a758d8e76
akhilprasad94 <prasad.ak@husky.neu.edu>
2019-07-15 21:11:41 -0400

    Added changes

--- infrastructure/aws/codedeploy/applicationstart.sh
+++ infrastructure/aws/codedeploy/applicationstart.sh
@@ -2,7 +2,7 @@
 tail /etc/profile -n 5 > /tmp/tempFile.sh
 sudo chmod 777 /tmp/tempFile.sh
 echo "sudo yum install lsof -y" >> /tmp/tempFile.sh
-echo "APP_PORT=sudo lsof -t -i:8080" >> /tmp/tempFile.sh
+echo "APP_PORT=sudo lsof -t -i:80" >> /tmp/tempFile.sh
 echo "sudo kill -9 \$APP_PORT" >> /tmp/tempFile.sh
 echo "cd /var" >> /tmp/tempFile.sh
 echo "sudo java -jar -Dspring.profiles.active=cloud -Ddatabase=\$DB_HOST -Dbucketname=\$S3_BUCKET LMSApp-0.0.1-SNAPSHOT.war" >> /tmp/tempFile.sh
