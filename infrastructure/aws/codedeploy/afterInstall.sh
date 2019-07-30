1083199891527743ca109688afc7636ecb5446b8
Kiranzigma <kathiresan.k@husky.neu.edu>
2019-07-30 12:11:50 -0400

    Changes

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -2,7 +2,7 @@
 
 sudo bash /opt/tomcat/bin/shutdown.sh
 
-#sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
+sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
 
 #sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 
